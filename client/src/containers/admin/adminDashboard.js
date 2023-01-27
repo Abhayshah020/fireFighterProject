f57120b20accf34c94cfdd527cc1f4018fdded97
unknown <abhayshah020>
2023-01-27 14:31:46 +0545

    Created skeleton component But not used and deleted empty files

--- client/src/containers/admin/adminDashboard.js
+++ client/src/containers/admin/adminDashboard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
-
- const Home = () => {
-    const {_id,name} =useSelector(state=>state.user)
-    return(
+const Home = () => {
+    const { _id } = useSelector(state => state.user)
+    const { name, role } = useSelector(state => state.user)
+    return (
         <>
-        <h1>Welcom Admin {name}. Your Id is #{_id}</h1>
+            <h1>Welcom Admin {name}. Your Id is #{_id}</h1>
         </>
     )
- }
- export default Home;
+}
+export default Home;
--- client/src/containers/user/userDashboard.js
+++ client/src/containers/user/userDashboard.js
@@ -1,16 +1,8 @@
 import { useSelector } from 'react-redux'
-import { logoutResetDetails } from "../../redux/actions/userAction"
-import { useDispatch } from 'react-redux'
-import CardPage from '../../components/cardPage'
-import Footer from '../../components/footer';
 
 function UserDashboard() {
   const {_id} =useSelector(state=>state.user)
-  const dispatch = useDispatch()
   const { name,role } = useSelector(state => state.user)
-  const triggerLogout = () => {
-      dispatch(logoutResetDetails())
-  }
   return (
     <>
     <div>
